cec9e95138854ebe8a94c44207d702ae6c08b918
Prasanna Rednam <prasannarednam08@gmail.com>
2026-05-15 23:48:04 +0530

    action

--- main.tf
+++ main.tf
@@ -11,3 +11,13 @@ resource "aws_internet_gateway" "main" {
 
   tags = local.igw_final_tags
 }
+# Public subnets
+resource "aws_subnet" "public" {
+    count = lenght(var.public_subnet_cidrs)
+    vpc_id     = aws_vpc.main.id
+    cidr_block = var.public_subnet_cidrs[count.index]
+
+    tags = {
+        Name = "Main"
+  }
+}
